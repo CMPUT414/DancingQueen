93c835ebbf91b41286916d1041f423fb2a44c6cb
sensible-heart <alexandra.makepeace@gmail.com>
2016-03-26 16:52:29 -0600

    Added a test .wav file.

--- BPMDetector.py
+++ BPMDetector.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python
 import wave, array, math, time, argparse, sys
 import numpy, pywt
 from scipy import signal
@@ -27,24 +28,24 @@ def read_wav(filename):
         assert(nsamps == len(samps))
     except AssertionError, e:
         print  nsamps, "not equal to", len(samps)
-    
+
     return samps, fs
-    
+
 # print an error when no data can be found
 def no_audio_data():
     print "No audio data for sample, skipping..."
     return None, None
-    
+
 # simple peak detection
 def peak_detect(data):
-    max_val = numpy.amax(abs(data)) 
+    max_val = numpy.amax(abs(data))
     peak_ndx = numpy.where(data==max_val)
     if len(peak_ndx[0]) == 0: #if nothing found then the max must be negative
         peak_ndx = numpy.where(data==-max_val)
     return peak_ndx
-    
+
 def bpm_detector(data,fs):
-    cA = [] 
+    cA = []
     cD = []
     correl = []
     cD_sum = []
@@ -52,7 +53,7 @@ def bpm_detector(data,fs):
     max_decimation = 2**(levels-1);
     min_ndx = 60./ 220 * (fs/max_decimation)
     max_ndx = 60./ 40 * (fs/max_decimation)
-    
+
     for loop in range(0,levels):
         cD = []
         # 1) DWT
@@ -71,34 +72,34 @@ def bpm_detector(data,fs):
         cD = abs(cD[::(2**(levels-loop-1))]);
         cD = cD - numpy.mean(cD);
         # 6) Recombine the signal before ACF
-        #    essentially, each level I concatenate 
+        #    essentially, each level I concatenate
         #    the detail coefs (i.e. the HPF values)
         #    to the beginning of the array
         cD_sum = cD[0:cD_minlen] + cD_sum;
 
     if [b for b in cA if b != 0.0] == []:
         return no_audio_data()
-    # adding in the approximate data as well...    
+    # adding in the approximate data as well...
     cA = signal.lfilter([0.01],[1 -0.99],cA);
     cA = abs(cA);
     cA = cA - numpy.mean(cA);
     cD_sum = cA[0:cD_minlen] + cD_sum;
-    
+
     # ACF
-    correl = numpy.correlate(cD_sum,cD_sum,'full') 
-    
+    correl = numpy.correlate(cD_sum,cD_sum,'full')
+
     midpoint = len(correl) / 2
     correl_midpoint_tmp = correl[midpoint:]
     peak_ndx = peak_detect(correl_midpoint_tmp[min_ndx:max_ndx]);
     if len(peak_ndx) > 1:
         return no_audio_data()
-        
+
     peak_ndx_adjusted = peak_ndx[0]+min_ndx;
     bpm = 60./ peak_ndx_adjusted * (fs/max_decimation)
     print bpm
     return bpm,correl
-    
-    
+
+
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Process .wav file to determine the Beats Per Minute.')
     parser.add_argument('--filename', required=True,
@@ -108,14 +109,14 @@ if __name__ == '__main__':
 
     args = parser.parse_args()
     samps,fs = read_wav(args.filename)
-    
+
     data = []
     correl=[]
     bpm = 0
     n=0;
     nsamps = len(samps)
-    window_samps = int(args.window*fs)         
-    samps_ndx = 0;  #first sample in window_ndx 
+    window_samps = int(args.window*fs)
+    samps_ndx = 0;  #first sample in window_ndx
     max_window_ndx = nsamps / window_samps;
     bpms = numpy.zeros(max_window_ndx)
 
@@ -126,23 +127,23 @@ if __name__ == '__main__':
         #print n,":",len(bpms),":",max_window_ndx,":",fs,":",nsamps,":",samps_ndx
         data = samps[samps_ndx:samps_ndx+window_samps]
         if not ((len(data) % window_samps) == 0):
-            raise AssertionError( str(len(data) ) ) 
-        
+            raise AssertionError( str(len(data) ) )
+
         bpm, correl_temp = bpm_detector(data,fs)
         if bpm == None:
             continue
         bpms[window_ndx] = bpm
         correl = correl_temp
-        
+
         #iterate at the end of the loop
         samps_ndx = samps_ndx+window_samps;
         n=n+1; #counter for debug...
 
     bpm = numpy.median(bpms)
     print 'Completed.  Estimated Beats Per Minute:', bpm
-    
+
     n = range(0,len(correl))
-    plt.plot(n,abs(correl)); 
+    plt.plot(n,abs(correl));
     plt.show(False); #plot non-blocking
     time.sleep(10);
     plt.close();
